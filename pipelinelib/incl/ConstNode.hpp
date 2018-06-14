f1b1fa5bc1ac73828d1dd7676bdbafe88d25a83a
lorinc <serfozo.lorinc@gmail.com>
2018-06-14 16:35:50 +0200

    using kaguya instead of selene, lua reflection

--- pipelinelib/incl/ConstNode.hpp
+++ pipelinelib/incl/ConstNode.hpp
@@ -9,15 +9,24 @@ namespace Pipeline {
 template<typename T>
 class ConstNode : public Node<tuple<>, tuple<T>> {
 public:
+    ConstNode() : m_data()
+    {
+    }
     explicit ConstNode(T&& data) : m_data(data)
     {
     }
     tuple<unique_ptr<T>> process(const tuple<>& inData) const override {
         return tuple<unique_ptr<T>>{ std::make_unique<T>(m_data) };
     }
+    const T& getData() const {
+        return m_data;
+    }
+    void setData(const T& data) {
+        m_data = data;
+    }
 
 private:
-    T const m_data;
+    T m_data;
 };
 
 }
